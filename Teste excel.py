2a520bc614fc00156394c345d63d90cbc55275bb
Paola Miranda <88098114+pamiranda@users.noreply.github.com>
2022-01-28 10:16:39 -0300

    Add files via upload

---
+++ Teste excel.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+#Abei uma planilha de excel do trabalho usando a função do pandas
+import pandas as pd
+dados = pd.read_excel(r"C:\Users\paola.miranda\Documents\didaticatech\SR.VAREJISTA.xlsx")
+
+
+# In[14]:
+
+
+dados.shape
+
+
+# In[15]:
+
+
+dados.head()
+
+
+# In[ ]:
+
+
+
+
