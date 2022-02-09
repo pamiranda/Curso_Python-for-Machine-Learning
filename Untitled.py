02c9560e6b4810789506491563361e48867e999d
Paola Miranda <88098114+pamiranda@users.noreply.github.com>
2022-02-09 11:10:50 -0300

    Conceito de classes em Python

---
+++ Untitled.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+class ControleRemoto:
+    def __init__(self):
+        cor = 'preto'
+        altura = '2cm'
+        altura = '2cm'
+        largura = '10cm'
+        
+controle_remoto = ControleRemoto()
+  
+
+
+# In[7]:
+
+
+type(ControleRemoto)
+
+
+# In[ ]:
+
+
+
+
